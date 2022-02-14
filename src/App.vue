b8ddd05a46ea26e18a49019185275f3136a3b8fe
marcodeiulio <mdeiulio@icloud.com>
2022-02-14 16:12:41 +0100

    - Added axios package - Added v-model searchQuery - Added fetchMovies()

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <input type="text" placeholder="search query" v-model="searchQuery" />
+    <button id="submit-query" @click="fetchMovies">Search</button>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      searchQuery: "",
+      api: {
+        moviesEndpoint: "search/movie?",
+        seriesEndpoint: "",
+        api_key: "api_key=3f606812feca15a95aae9e1e4b7e1f3b",
+      },
+      movies: [],
+    };
+  },
+  methods: {
+    fetchMovies() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/${this.api.moviesEndpoint}${this.api.api_key}&query=${this.searchQuery}`
+        )
+        .then((res) => {
+          this.movies.push(res.data.results);
+          // console.log(res.data.results);
+          console.log(this.movies);
+        });
+    },
+  },
 };
 </script>
 
